ccb21184a3baf94b89f865d63890419a05f8c0c2
sagarnayakanti <44189540+sagarnayakanti@users.noreply.github.com>
2018-10-16 12:47:22 +0530

    Add files via upload

---
+++ ExampleTask/ExampleTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleTask
+//
+//  Created by Sagar on 10/16/18.
+//  Copyright © 2018 Sagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var name = "HELLO WORLD"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(name)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
